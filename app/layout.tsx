4fedc19f7fe87537e1e9deed9f8b1e8067b53395
kottagyula <kottagyula@gmail.com>
2026-09-08 16:57:21 +0200

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
-import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Vince Medence - Magiline Specialista",
   description: "Közel 10 éves szakértői múlt a Magiline medencék karbantartásában.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="hu">
       <body>{children}</body>
